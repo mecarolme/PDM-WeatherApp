4f5e898695fc165d38c8ddb4b266e600b4b45ae1
cmn <cmn@discente.ifpe.edu.br>
2024-12-16 19:45:03 -0300

    Prática 01 – Parte 4 – Registro

--- app/src/main/java/com/weatherapp/RegisterActivity.kt
+++ app/src/main/java/com/weatherapp/RegisterActivity.kt
@@ -1,36 +1,44 @@
 package com.weatherapp
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.weatherapp.ui.theme.WeatherAppTheme
-import android.app.Activity
-import android.content.Intent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.material3.*
-import androidx.compose.ui.Alignment
+import com.weatherapp.ui.theme.WeatherAppTheme
 
-class LoginActivity : ComponentActivity() {
+class RegisterActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             WeatherAppTheme {
-                LoginPage()
+                RegisterPage()
             }
         }
     }
@@ -38,20 +46,30 @@ class LoginActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun LoginPage(modifier: Modifier = Modifier) {
+fun RegisterPage(modifier: Modifier = Modifier) {
+    var name by rememberSaveable { mutableStateOf("") }
     var email by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
+    var passwordConf by rememberSaveable { mutableStateOf("") }
+
     val activity = LocalContext.current as? Activity
+
     Column(
         modifier = modifier.padding(16.dp).fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Bem-vindo/a!",
+            text = "Cadastre-se",
             fontSize = 24.sp
         )
         Spacer(modifier = modifier.size(24.dp))
+        OutlinedTextField(
+            value = name,
+            label = { Text(text = "Digite seu nome") },
+            modifier = modifier.fillMaxWidth(),
+            onValueChange = { name = it }
+        )
         OutlinedTextField(
             value = email,
             label = { Text(text = "Digite seu e-mail") },
@@ -65,36 +83,33 @@ fun LoginPage(modifier: Modifier = Modifier) {
             onValueChange = { password = it },
             visualTransformation = PasswordVisualTransformation()
         )
+        OutlinedTextField(
+            value = passwordConf,
+            label = { Text(text = "Confirme sua senha") },
+            modifier = modifier.fillMaxWidth(),
+            onValueChange = { passwordConf = it },
+            visualTransformation = PasswordVisualTransformation()
+        )
         Spacer(modifier = modifier.size(24.dp))
         Row(modifier = modifier) {
             Button(
                 onClick = {
+                    Toast.makeText(activity, "Registro realizado com sucesso!", Toast.LENGTH_LONG).show()
+
                     activity?.startActivity(
-                        Intent(activity, MainActivity::class.java).setFlags(
+                        Intent(activity, LoginActivity::class.java).setFlags(
                             Intent.FLAG_ACTIVITY_SINGLE_TOP
                         )
                     )
 
+                    activity?.finish()
                 },
-                enabled = email.isNotEmpty() && password.isNotEmpty()
+                enabled = name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && passwordConf.isNotEmpty() && password == passwordConf
             ) {
-                Text("Login")
+                Text("Registrar")
             }
-
             Button(
-                onClick = {
-                    activity?.startActivity(
-                        Intent(activity, RegisterActivity::class.java).setFlags(
-                            Intent.FLAG_ACTIVITY_SINGLE_TOP
-                        )
-                    )
-                }
-            ) {
-                Text("Registro")
-            }
-
-            Button(
-                onClick = { email = ""; password = "" }
+                onClick = { name = ""; email = ""; password = ""; passwordConf = "" }
             ) {
                 Text("Limpar")
             }
